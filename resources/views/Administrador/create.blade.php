14ff49162eb21d7f478906e45d0222389e856a2c
NaoSalgado <anghelasalgadof@gmail.com>
2025-06-03 22:39:28 -0600

    Corrección gitignore y vista blade de login y crear administrador

---
+++ resources/views/Administrador/create.blade.php
@@ -0,0 +1,25 @@
+@extends('template')
+
+@section('title', 'Crear Administrador')
+@push('css')
+    <link href="{{ asset('css/crearAdmin.css') }}" rel="stylesheet" />
+@endpush
+@section('content')
+  <div class="form-center-container">
+    <div class="form-container text-center">
+      <h3>Crear Administrador</h3>
+      <form>
+          <div class="form-floating mb-3 w-100">
+            <input type="text" class="form-control" placeholder="Ingrese el nombre de usuario">
+            <label for="floatingUser">Ingrese su usuario</label>
+          </div>
+          <div class="form-floating mb-3 w-100">
+            <input type="password" class="form-control" placeholder="Ingrese una contraseña">
+            <label for="floatingPassword">Ingrese su contraseña</label>
+          </div>
+        <div class="w-100 d-flex justify-content-end">
+          <button type="submit" class="btn btn-accept">ACEPTAR</button>
+        </div>
+      </form>
+    </div>
+
